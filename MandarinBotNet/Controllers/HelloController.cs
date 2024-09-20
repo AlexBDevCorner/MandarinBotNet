6d7fb1546f013005892e0b9bdcc4de913caf64df
Bobrovs, Aleksandrs <aleksandrs.bobrovs@if.lv>
2024-09-20 16:33:36 +0300

    Added dummy controller

---
+++ MandarinBotNet/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MandarinBotNet.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello!");
+        }
+    }
+}
